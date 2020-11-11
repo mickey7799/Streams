ed12103f91755a9e401b3d7cfe5bfce8fbfdf57f
mickey7799 <vickeylee.w@gmail.com>
2020-11-11 18:04:38 +1100

    Added streaming function

--- client/src/components/streams/StreamList.js
+++ client/src/components/streams/StreamList.js
@@ -28,18 +28,20 @@ class StreamList extends React.Component {
 
   renderList() {
     return this.props.streams.map(stream => {
-      return (
-        <div className='item' key={stream.id}>
-          {this.renderAdmin(stream)}
-          <i className='large middle aligned icon camera' />
-          <div className='content'>
-            <Link to={`/streams/${stream.id}`} className='header'>
-              {stream.title}
-            </Link>
-            <div className='description'>{stream.description}</div>
+      if (stream.userId === this.props.currentUserId) {
+        return (
+          <div className='item' key={stream.id}>
+            {this.renderAdmin(stream)}
+            <i className='large middle aligned icon camera' />
+            <div className='content'>
+              <Link to={`/streams/${stream.id}`} className='header'>
+                {stream.title}
+              </Link>
+              <div className='description'>{stream.description}</div>
+            </div>
           </div>
-        </div>
-      );
+        );
+      }
     });
   }
 
